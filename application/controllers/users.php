698507d745e9164d954c2d66d677c66f7eca5cd1
Dana Sugu <dana.sugu@gmail.com>
2021-11-11 17:15:55 +0200

    update

--- application/controllers/users.php
+++ application/controllers/users.php
@@ -6,7 +6,9 @@ class Users extends CI_Controller {
 
 
     public function show(){
-        $this->load->model('user_model');
+        // $this->load->model('user_model'); not needed if activated in autoload $autoload['model'] = array('user_model');
+
+        //get an aray of objects from db
         $result = $this->user_model->get_users();
 
     }
--- application/models/user_model.php
+++ application/models/user_model.php
@@ -4,5 +4,14 @@
 
 class User_model extends CI_Model {
 
+    public function get_users() {
+    $query = $this->db->get('users');
+
+    //return an array of objects
+    return $query -> result();
+
+
+
+    }
 
 }
